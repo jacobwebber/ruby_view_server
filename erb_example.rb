299b1b8872c2c826ceeb1dfada5e1409ff823e87
Jake <jwebb8207@gmail.com>
2014-02-17 20:54:54 +1000

    i Experimented with an ERB file and added some array functionality.

--- erb_example.rb
+++ erb_example.rb
@@ -1,5 +1,9 @@
 require 'erb'
 
-x = 42
-template = ERB.new "The value of x is: <%= x %>"
+dogs = ['bronson', 'gordy']
+
+d1 = dogs[0]
+d2 = dogs[1]
+
+template = ERB.new "I have two dogs named <%= d1 %> and <%= d2 %>."
 puts template.result(binding)
